8db16eb7bd6ed7b284fe209e542a186d71388a27
Josef Biehler <gabbersepp@googlemail.com>
2019-06-13 22:31:46 +0200

    add command line interface

--- Sources/BfCompiler/BfCompiler/Program.cs
+++ Sources/BfCompiler/BfCompiler/Program.cs
@@ -1,12 +1,33 @@
-﻿namespace BfCompiler
+﻿using System.IO;
+
+namespace BfCompiler
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var bfCode = "++[>++++++++++++++++++++[>+<-]<-]>>.";// = "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.";
+            string code = "";
 
-            new BfCompiler(bfCode).Compile("bf.exe");
+            if (args.Length == 0)
+            {
+                code = "++[>++++++++++++++++++++[>+<-]<-]>>.";
+            }
+            else if (args[0] == "-f")
+            {
+                code = ReadFile(args[1]);
+            }
+            else if(args[0] == "-c")
+            {
+                code = args[1];
+            }
+            //var bfCode = ;// = "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.";
+
+            new BfCompiler(code).Compile("bf.exe");
+        }
+
+        private static string ReadFile(string path)
+        {
+            return File.ReadAllText(path);
         }
     }
 }
